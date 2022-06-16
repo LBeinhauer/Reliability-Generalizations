d910a2177b9d11151bb9a65beac3df435d328ec8
lukas.beinhauer <lukas.beinhauer@gmail.com>
2022-06-16 15:39:15 +0200

    Update graphics

--- temp_Graphics_script.R
+++ temp_Graphics_script.R
@@ -414,6 +414,81 @@ summary(alpha_tau)
 
 
 
+# Plots without Bonett-Transformed estimates:
+
+violin_I2_lonely <- violin_df[which(violin_df$stat == 1),] %>%
+  ggplot() + 
+  geom_violin(aes(x = stat, y = I2), fill = "transparent") +
+  geom_boxplot(aes(x = stat, y = I2), width = .1, fill = "transparent") +
+  geom_point(aes(x = stat, y = I2, shape = sig), colour =  "#f66963",
+             position = position_jitter(w = 0.1, h = 0), size = 4, alpha = .7) +
+  scale_x_discrete(labels = c("")) +
+  scale_shape_manual(values = c(21, 16)) +
+  theme(legend.position = "none",
+        text = element_text(size = 17),
+        panel.background = element_rect(fill = "transparent",
+                                        colour = NA_character_), 
+        panel.grid.major.y = element_line(colour = "grey"),
+        panel.grid.major.x = element_line(colour = "transparent"),
+        panel.grid.minor.x = element_line(colour = "transparent"),
+        panel.grid.minor.y = element_line(colour = "transparent"),
+        axis.line.x = element_line(colour = "transparent"),
+        axis.line.y = element_line(colour = "transparent"),
+        axis.ticks.x = element_line(colour = "transparent"),
+        axis.ticks.y = element_line(colour = "grey"),
+        plot.background = element_rect(fill = "transparent", colour = NA)) +
+  labs(x = "", y = expression(~I^2), title = "")
+
+
+v1 <- violin_df[which(violin_df$stat == 1),] %>%
+  ggplot() + 
+  geom_violin(aes(x = 0, y = tau), fill = "transparent") +
+  geom_boxplot(aes(y = tau), width = .1, fill = "transparent") +
+  geom_point(aes(x = 0, y = tau, shape = sig),  colour =  "#f66963",
+             position = position_jitter(w = 0.1, h = 0), size = 4, alpha = .7) +
+  scale_x_discrete(labels = c("")) +
+  scale_shape_manual(values = c(21, 16)) +
+  theme(legend.position = "none",
+        text = element_text(size = 17),
+        panel.background = element_rect(fill = "transparent",
+                                        colour = NA_character_), 
+        panel.grid.major.y = element_line(colour = "grey"),
+        panel.grid.major.x = element_line(colour = "transparent"),
+        panel.grid.minor.x = element_line(colour = "transparent"),
+        panel.grid.minor.y = element_line(colour = "transparent"),
+        axis.line.x = element_line(colour = "transparent"),
+        axis.line.y = element_line(colour = "transparent"),
+        axis.ticks.x = element_line(colour = "transparent"),
+        axis.ticks.y = element_line(colour = "grey"),
+        plot.background = element_rect(fill = "transparent", colour = NA)) +
+  labs(x = "", y = expression(~tau), title = "") 
+
+
+grid_lonely <- grid.arrange(violin_I2_lonely, v1, ncol = 2,
+                            top=textGrob("Heterogeneity in Cronbach's Alpha", gp=gpar(fontsize=22)))
+
+# ggsave(
+#   plot = grid_lonely,
+#   filename = here("Graphics/temp_ViolinPlots_lonely_Alpha.svg"),
+#   bg = "transparent",
+#   width = 6,
+#   height = 6,
+#   units = "in"
+# )
+
+
+svg(here("Graphics/temp_ViolinPlots_lonely_Alpha.svg"), bg = "transparent",
+        width = 6, height = 6)
+
+grid.arrange(violin_I2_lonely, v1, ncol = 2,
+             top=textGrob("Heterogeneity in Cronbach's Alpha", gp=gpar(fontsize=22)))
+
+dev.off()
+
+
+
+
+
 
 
 ########################################################
@@ -465,12 +540,49 @@ violin_estimates <- gridExtra::grid.arrange(v3, v4, ncol = 2, top = "Violin Plot
 
 
 
+# plots without Bonett-transformed estimates
 
 
+v3_lonely <- violin_df[which(violin_df$stat == 1),] %>%
+  ggplot() + 
+  geom_violin(aes(x = 0, y = est), fill = "transparent") +
+  geom_boxplot(aes(y = est), width = .1, fill = "transparent") +
+  geom_point(aes(x = 0, y = est), colour = gg_color_hue(2)[1], 
+             position = position_jitter(w = 0.1, h = 0), size = 4, alpha = .7) +
+  scale_x_discrete(labels = c("")) +
+  theme(legend.position = "none",
+        text = element_text(size = 17),
+        panel.background = element_rect(fill = "transparent",
+                                        colour = NA_character_), 
+        panel.grid.major.y = element_line(colour = "grey"),
+        panel.grid.major.x = element_line(colour = "transparent"),
+        panel.grid.minor.y = element_line(colour = "transparent"),
+        panel.grid.minor.x = element_line(colour = "transparent"),
+        axis.line.x = element_line(colour = "transparent"),
+        axis.line.y = element_line(colour = "transparent"),
+        axis.ticks.x = element_line(colour = "transparent"),
+        axis.ticks.y = element_line(colour = "grey"),
+        plot.background = element_rect(fill = "transparent", colour = NA)) +
+  labs(x = "", y = "Cronnbach's Alpha") 
 
 
+violin_estimates_lonely <- gridExtra::grid.arrange(v3_lonely, top = textGrob("Reliability Estimates", gp=gpar(fontsize=22)))
 
+svg(here("Graphics/temp_ViolinPlots_estimates_lonely_Alpha.svg"), bg = "transparent",
+        width = 4, height = 4)
 
+gridExtra::grid.arrange(v3_lonely, top = textGrob("Reliability Estimates", gp=gpar(fontsize=22)))
+
+dev.off()
+
+# ggsave(
+#   plot = violin_estimates_lonely,
+#   filename = here("Graphics/temp_ViolinPlots_estimates_lonely_Alpha.svg"),
+#   bg = "transparent",
+#   width = 4,
+#   height = 4,
+#   units = "in"
+# )
 
 
 ####################################################################
@@ -568,6 +680,81 @@ violin_df[which(violin_df$stat == 2),] %>%
 ##############################################################################################
 
 
+my_forest_plot <- function(rma.fit, rma.data, main.title = "Forest Plot", 
+                           x.lab = "Estimate", ci.lvl = .975, CI.display = FALSE){
+  
+  # Calculate lower and upper limits of confidence levels, for each replication's estimate
+  cil <- rma.fit$yi[1:length(rma.fit$yi)] - qnorm(ci.lvl)*sqrt(rma.fit$vi[1:length(rma.fit$vi)])
+  ciu <- rma.fit$yi[1:length(rma.fit$yi)] + qnorm(ci.lvl)*sqrt(rma.fit$vi[1:length(rma.fit$vi)])
+  
+  
+  # weights <- 1/sqrt(rma.fit$vi+rma.fit$tau2)
+  # weights.scaled <- weights/mean(weights)
+  # weights.rescaled <- weights.scaled/mean(weights.scaled)
+  
+  p <- ggplot() + # initialize ggplot
+    
+    # plot point estimates
+    geom_point(aes(x = rma.fit$yi, y = c(5:(length(rma.fit$yi)+4))
+                   #                , size = weights.rescaled
+    ), shape = 15) + 
+    
+    # vertical line at x = 0
+    # geom_vline(xintercept = 0, linetype = "dashed") +
+    
+    # add horizontal line for CI of each replication's estimate
+    geom_segment(aes(x = cil, y = c(5:(length(rma.fit$yi)+4)), xend = ciu, yend = c(5:(length(rma.fit$yi)+4)))) +
+    
+    # ggplot theme
+    theme_minimal() +
+    
+    # plot meta analytic point estimate
+    geom_point(aes(x = rma.fit$b[1], y = 1), shape = 18) +
+    
+    #add CI-line for meta-analytic point estimate
+    geom_segment(aes(x = rma.fit$b[1] - qnorm(ci.lvl)*rma.fit$se, y = 1, 
+                     xend = rma.fit$b[1] + qnorm(ci.lvl)*rma.fit$se, yend = 1)) +
+    
+    # add vertical upper & lower-limit "fence"-lines, for each replication's estimate
+    geom_segment(aes(x = cil, xend = cil, y = (c(5:(length(rma.fit$yi)+4))+.3), yend = (c(5:(length(rma.fit$yi)+4))-.3) )) +
+    geom_segment(aes(x = ciu, xend = ciu, y = (c(5:(length(rma.fit$yi)+4))+.3), yend = (c(5:(length(rma.fit$yi)+4))-.3) )) +
+    
+    # add vertical upper- & lower-limit "fence lines, for meta-analytic point estimate
+    geom_segment(aes(x = rma.fit$b[1] - qnorm(ci.lvl)*rma.fit$se, y = (1+.3), 
+                     xend = rma.fit$b[1] - qnorm(ci.lvl)*rma.fit$se, yend = (1-.3))) +
+    geom_segment(aes(x = rma.fit$b[1] + qnorm(ci.lvl)*rma.fit$se, y = (1+.3), 
+                     xend = rma.fit$b[1] + qnorm(ci.lvl)*rma.fit$se, yend = (1-.3))) +
+    
+    
+    
+    
+    # labs & titles
+    xlab(x.lab) +
+    ylab("Lab") # +
+    # ggtitle(main.title)
+  
+  if(CI.display){
+    p <- p + 
+      scale_y_continuous(breaks = c(1, (5:(length(rma.fit$yi)+4))), 
+                         labels = c("RE Model", unique(as.character(rma.data$source))),
+                         
+                         sec.axis = dup_axis(breaks = c(1, (5:(length(rma.fit$yi)+4))),
+                                             labels = c(paste0("[", round(rma.fit$b[1] - qnorm(ci.lvl)*rma.fit$se, 2), ";", round(rma.fit$b[1] + qnorm(ci.lvl)*rma.fit$se, 2), "]"), 
+                                                        paste0("[", round(cil, 2), ";", round(ciu, 2), "]")),
+                                             name = ""))
+    # p <- p + geom_text(aes(y = c(5:(length(rma.fit$yi)+4)), x = (max(ciu) + abs(max(ciu))*.05),
+    #               label = paste0("[", round(cil, 2), ";", round(ciu, 2), "]")))
+  }else{
+    p <- p +     # adjust labels on y-axis, to display lab-abreviations
+      scale_y_continuous(breaks = c(1, (5:(length(rma.fit$yi)+4))), labels = c("RE Model", unique(as.character(rma.data$source)))                         ) 
+  }
+  
+  p
+}
+
+
+
+
 laymat <- matrix(c(1, 1, 1, 1, 2, 2, 2,
                    1, 1, 1, 1, 2, 2, 2,
                    1, 1, 1, 1, 2, 2, 2,
@@ -580,11 +767,23 @@ AE <- Alpha_estimates.list[[27]]
 AR <- Alpha_rma.list[[27]]
 
 
-p <- my_forest_plot(rma.data = AE, rma.fit = AR,
-                    main.title = paste0("Forest Plot - ", substr(names(data.list),
-                                                                 (regexpr("Project) Data/", names(data.list)) + 14),
-                                                                 (nchar(names(data.list))-4))[27]),
+p <- my_forest_plot(rma.data = AE, rma.fit = AR, main.title = NULL,
                     x.lab = "Cronbach's Alpha", ci.lvl = .975, CI.display = FALSE)
+
+p <- p + theme(panel.background = element_rect(fill = "transparent",
+                                               colour = NA_character_), 
+               text = element_text(size = 17),
+               panel.grid.major.y = element_line(colour = "grey"),
+               panel.grid.major.x = element_line(colour = "grey"),
+               panel.grid.minor.y = element_line(colour = "transparent"),
+               panel.grid.minor.x = element_line(colour = "transparent"),
+               axis.line.x = element_line(colour = "transparent"),
+               axis.line.y = element_line(colour = "transparent"),
+               axis.ticks.x = element_line(colour = "transparent"),
+               axis.ticks.y = element_line(colour = "grey"),
+               plot.background = element_rect(fill = "transparent", colour = NA),
+               axis.text.y = element_text(size = 8)) 
+  
 
 
 bw_FD <- (2 * IQR(AE$Reliability))/length(AE$Reliability)^(1/3)
@@ -592,24 +791,50 @@ bw_FD <- (2 * IQR(AE$Reliability))/length(AE$Reliability)^(1/3)
 h <- ggplot(AE) +
   geom_histogram(aes(x = Reliability), 
                  binwidth = bw_FD,
-                 colour = "black", fill = gg_color_hue(2)[1]) +
-  theme(panel.background = element_rect(fill = "white"), 
+                 colour = "black", fill = "#f66963") +
+  theme(panel.background = element_rect(fill = "transparent",
+                                        colour = NA_character_), 
+        text = element_text(size = 17),
         panel.grid.major.y = element_line(colour = "grey"),
-        panel.grid.major.x = element_line(colour = "grey"),
-        axis.line.x = element_line(colour = "white"),
-        axis.line.y = element_line(colour = "white"),
-        axis.ticks.x = element_line(colour = "grey"),
-        axis.ticks.y = element_line(colour = "grey")) +
-  labs(x = "Cronbach's Alpha", y = "Frequency", title = paste0("Histogram - ", substr(names(data.list),
-                                                                                      (regexpr("Project) Data/", names(data.list)) + 14),
-                                                                                      (nchar(names(data.list))-4))[27]))
+        panel.grid.major.x = element_line(colour = "transparent"),
+        panel.grid.minor.y = element_line(colour = "transparent"),
+        panel.grid.minor.x = element_line(colour = "transparent"),
+        axis.line.x = element_line(colour = "transparent"),
+        axis.line.y = element_line(colour = "transparent"),
+        axis.ticks.x = element_line(colour = "transparent"),
+        axis.ticks.y = element_line(colour = "grey"),
+        plot.background = element_rect(fill = "transparent", colour = NA)) +
+  labs(x = "Cronbach's Alpha", y = "Frequency")
 
 
 
 
-gridExtra::grid.arrange(p, h, layout_matrix = laymat)
+Forest_Histogram_HH <- gridExtra::grid.arrange(p, h, layout_matrix = laymat, 
+                                               top = textGrob("Reliability Estimates - HEXACO HH", gp=gpar(fontsize=22)))
+
+
+ggsave(
+  plot = Forest_Histogram_HH,
+  filename = here("Graphics/temp_Forest_Histogram.svg"),
+  bg = "transparent",
+  width = 8.5,
+  height = 6.5,
+  units = "in"
+)
 
 
 
+## All Scales
+
+
+substr(names(data.list),
+       (regexpr("Project) Data/", names(data.list)) + 14),
+       (nchar(names(data.list))-4))
+
+unique(substr(scales_meta$Scale, 1, regexpr(" ", scales_meta$Scale) - 1))
+length(unique(substr(scales_meta$Scale, 1, regexpr(" ", scales_meta$Scale) - 1)))
+
+scales_meta <- read.csv(here("Notes/Tutzing_Vis_Scales.csv"))
+sum(scales_meta$Psychometrics)
 
 

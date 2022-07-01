9b95b365544d2b4f588d0c7da0c8ffb9d4184094
lukas.beinhauer <lukas.beinhauer@gmail.com>
2022-07-01 09:40:17 +0200

    Vplot heterogeneity omega

--- app.R
+++ app.R
@@ -32,6 +32,11 @@ Alpha_rma.list <- readRDS(here("Data/Shiny Data/Alpha_rma.list.RData"))[-7]
 
 Bonett.Alpha_rma.list <- readRDS(here("Data/Shiny Data/Bonett.Alpha_rma.list.RData"))
 
+Omega_rma.list <- readRDS(here("Data/Shiny Data/Omega_rma.list.RData"))
+
+Bonett.Omega_rma.list <- readRDS(here("Data/Shiny Data/Bonett.Omega_rma.list.RData"))
+
+
 Reliability_estimates_paths <- list.files(here("Data/Reliability Estimates"), full.names =  TRUE)
 
 Alpha_estimates.list <- Reliability_estimates_paths[grep("_Alpha.csv$", Reliability_estimates_paths)][-7] %>%
@@ -70,9 +75,11 @@ names(Bonett.Omega_estimates.list) <- substr(Reliability_estimates_paths[grep("_
 data.list <- list.files(here("Data/Extracted (Project) Data"), full.names = TRUE)[-7] %>%
     sapply(., read.csv)
 
-proj.names <- substr(names(data.list),
-                     (regexpr("Project) Data/", names(data.list)) + 14),
-                     (nchar(names(data.list))-4))
+names(data.list) <- substr(list.files(here("Data/Extracted (Project) Data"), full.names = FALSE)[-7],
+                           1, (nchar(list.files(here("Data/Extracted (Project) Data"), full.names = FALSE)[-7])-4))
+
+
+proj.names <- names(data.list)
 
 varT_rma.list <- readRDS(file = here("Notes/bootstrapped_varT_rma.RData"))[-7]
 
@@ -124,7 +131,9 @@ ui <- navbarPage(
             selectInput(inputId = "RelMeasFP",
                         label = "Reliability Measure",
                         choices = c("Cronbach's Alpha",
-                                    "Bonett-transf. Alpha")),
+                                    "McDonald's Omega",
+                                    "Bonett-transf. Alpha",
+                                    "Bonett-transf. Omega")),
             
             checkboxInput(inputId = "CI.bool", 
                           label = "Display Confidence-Interval",
@@ -151,7 +160,9 @@ ui <- navbarPage(
             selectInput(inputId = "RelMeasRMA",
                         label = "Reliability Measure",
                         choices = c("Cronbach's Alpha", 
-                                    "Bonett-transf. Alpha"))
+                                    "McDonald's Omega",
+                                    "Bonett-transf. Alpha",
+                                    "Bonett-transf. Omega"))
         ),
         
         mainPanel(
@@ -176,7 +187,7 @@ ui <- navbarPage(
         
         mainPanel(
             h4("Violin Plots for Heterogeneity"),
-            plotOutput(outputId = "violplot")
+            plotOutput(outputId = "violplot", height = "600px", width = "600px")
         )
         
         
@@ -197,7 +208,7 @@ ui <- navbarPage(
         
         mainPanel(
             h4("Forest Plots - Variance Decomposition"),
-            plotOutput(outputId = "VDforsplot")
+            plotOutput(outputId = "VDforsplot", height = "600px", width = "600px")
         )
     ),
     
@@ -236,17 +247,22 @@ server <- function(input, output) {
 
     output$forsplot <- renderPlot({
         
-        dl.index <- which(proj.names %in% input$ScaleFP)
-        
         if(input$RelMeasFP == "Cronbach's Alpha"){
-            data <- Alpha_rma.list[[dl.index]]
+            data <- Alpha_rma.list[[which(names(Alpha_rma.list) %in% input$ScaleFP)]]
+        }
+        if(input$RelMeasFP == "McDonald's Omega"){
+            data <- Omega_rma.list[[which(names(Omega_rma.list) %in% input$ScaleFP)]]
         }
         if(input$RelMeasFP == "Bonett-transf. Alpha"){
-            data <- Bonett.Alpha_rma.list[[dl.index]]
+            data <- Bonett.Alpha_rma.list[[which(names(Bonett.Alpha_rma.list) %in% input$ScaleFP)]]
         }
+        if(input$RelMeasFP == "Bonett-transf. Omega"){
+            data <- Bonett.Omega_rma.list[[which(names(Bonett.Omega_rma.list) %in% input$ScaleFP)]]
+        }
+       
         
         
-        forsplot <- my_forest_plot(rma.fit = data, rma.data = data.list[[dl.index]], 
+        forsplot <- my_forest_plot(rma.fit = data, rma.data = data.list[[which(names(data.list) %in% input$ScaleFP)]], 
                                    main.title = paste0("Forest Plot - ", input$ScaleFP),
                                    x.lab = input$RelMeasFP, CI.display = input$CI.bool)
         
@@ -286,13 +302,17 @@ server <- function(input, output) {
     
     output$REMA <- renderPlot({
         
-        dl.index <- which(proj.names %in% input$ScaleRMA)
-        
         if(input$RelMeasRMA == "Cronbach's Alpha"){
-            data <- Alpha_rma.list[[dl.index]]
+            data <- Alpha_rma.list[[which(names(Alpha_rma.list) %in% input$ScaleRMA)]]
+        }
+        if(input$RelMeasRMA == "McDonald's Omega"){
+            data <- Omega_rma.list[[which(names(Omega_rma.list) %in% input$ScaleRMA)]]
         }
         if(input$RelMeasRMA == "Bonett-transf. Alpha"){
-            data <- Bonett.Alpha_rma.list[[dl.index]]
+            data <- Bonett.Alpha_rma.list[[which(names(Bonett.Alpha_rma.list) %in% input$ScaleRMA)]]
+        }
+        if(input$RelMeasRMA == "Bonett-transf. Omega"){
+            data <- Bonett.Omega_rma.list[[which(names(Bonett.Omega_rma.list) %in% input$ScaleRMA)]]
         }
         
         
@@ -348,21 +368,62 @@ server <- function(input, output) {
             x$QEp <= .05
         })
         
+        
+        
+        
+        omega_tau <- sapply(Omega_rma.list, FUN = function(x){
+            sqrt(x$tau2)
+        })
+        
+        
+        omega_I2 <- sapply(Omega_rma.list, FUN = function(x){
+            x$I2
+        })
+        
+        omega_het.sig <- sapply(Omega_rma.list, FUN = function(x){
+            x$QEp <= .05
+        })
+        
+        
+        B.omega_tau <- sapply(Bonett.Omega_rma.list, FUN = function(x){
+            sqrt(x$tau2)
+        })
+        
+        # 1-exp(B.omega_tau)
+        
+        B.omega_I2 <- sapply(Bonett.Omega_rma.list, FUN = function(x){
+            x$I2
+        })
+        
+        B.omega_het.sig <- sapply(Bonett.Omega_rma.list, FUN = function(x){
+            x$QEp <= .05
+        })
+        
 
-        violin_df <- data.frame(tau = c(alpha_tau, B.alpha_tau),
-                                I2 = c(alpha_I2, B.alpha_I2),
-                                sig = c(alpha_het.sig, B.alpha_het.sig),
-                                stat = as.factor(c(rep(1, length(alpha_tau)), rep(2, length(alpha_tau)))))
+        violin_df <- data.frame(tau = c(alpha_tau, B.alpha_tau, omega_tau, B.omega_tau),
+                                I2 = c(alpha_I2, B.alpha_I2, omega_I2, B.omega_I2),
+                                sig = c(alpha_het.sig, B.alpha_het.sig, omega_het.sig, B.omega_het.sig),
+                                stat = as.factor(c(rep(0, length(alpha_tau)), rep(0, length(B.alpha_tau)),
+                                                   rep(1, length(omega_tau)), rep(1, length(B.omega_tau)))),
+                                transf = as.factor(c(rep(0, length(alpha_tau)), rep(1, length(B.alpha_tau)),
+                                                     rep(0, length(omega_tau)), rep(1, length(B.omega_tau)))))
         
         
         
         
         if(input$HetEst == "I2"){
             
+            row.labs <- c("untransformed", "Bonett-transf.")
+            names(row.labs) <- c("0", "1")
+            col.labs <- c("Cronbach's Alpha", "McDonald's Omega")
+            names(col.labs) <- c("0", "1")
+            
             vplot <- violin_df %>%
                 ggplot() + 
-                geom_violin(aes(x = stat, y = I2), fill = "transparent") +
-                geom_boxplot(aes(x = stat, y = I2), width = .1, fill = "transparent") +
+                facet_grid(rows = vars(transf), cols = vars(stat), scales = "free",
+                           labeller = labeller(transf = row.labs, stat = col.labs)) +
+                geom_violin(aes(x = 1, y = I2), fill = "transparent") +
+                geom_boxplot(aes(x = 1, y = I2), width = .1, fill = "transparent") +
                 scale_x_discrete(labels = c("Untransformed", "Bonett-Transformed")) +
                 theme(legend.position = "none",
                       panel.background = element_rect(fill = "transparent",
@@ -377,7 +438,7 @@ server <- function(input, output) {
             
             if(input$Sig.bool){
                 vplot <- vplot + 
-                    geom_point(aes(x = stat, y = I2, colour = stat, shape = sig), 
+                    geom_point(aes(x = 1, y = I2, colour = stat, shape = sig), 
                                position = position_jitter(w = 0.1, h = 0), size = 3, alpha = .7) +
                     scale_shape_manual(values = c(21, 16)) 
             }else{
@@ -394,8 +455,16 @@ server <- function(input, output) {
             
         
         if(input$HetEst == "tau"){
-            v1 <- violin_df[which(violin_df$stat == 1),] %>%
+            
+            row.labs <- c("untransformed", "Bonett-transf.")
+            names(row.labs) <- c("0", "1")
+            col.labs <- c("Cronbach's Alpha", "McDonald's Omega")
+            names(col.labs) <- c("0", "1")
+            
+            vplot <- violin_df %>%
                 ggplot() + 
+                facet_grid(rows = vars(transf), cols = vars(stat), scales = "free",
+                           labeller = labeller(transf = row.labs, stat = col.labs)) +
                 geom_violin(aes(x = 0, y = tau), fill = "transparent") +
                 geom_boxplot(aes(y = tau), width = .1, fill = "transparent") +
                 scale_x_discrete(labels = c("Untransformed")) +
@@ -411,47 +480,19 @@ server <- function(input, output) {
                 labs(x = "", y = "tau") +
                 scale_x_continuous(breaks = 0, labels = "Untransformed")
             
-            v2 <- violin_df[which(violin_df$stat == 2),] %>%
-                ggplot() + 
-                geom_violin(aes(x = 0, y = tau), fill = "transparent") +
-                geom_boxplot(aes(y = tau), width = .1, fill = "transparent") +
-                scale_x_discrete(labels = c("Bonett-transformed")) +
-                theme(legend.position = "none",
-                      panel.background = element_rect(fill = "transparent",
-                                                      colour = NA_character_), 
-                      panel.grid.major.y = element_line(colour = "grey"),
-                      panel.grid.major.x = element_line(colour = "transparent"),
-                      axis.line.x = element_line(colour = "transparent"),
-                      axis.line.y = element_line(colour = "black"),
-                      axis.ticks.x = element_line(colour = "transparent"),
-                      plot.background = element_rect(fill = "transparent", colour = NA)) +
-                labs(x = "", y = "tau") +
-                scale_x_continuous(breaks = 0, labels = "Bonett-Transformed")
+            
             
             if(input$Sig.bool){
-                v1 <- v1 + 
-                    geom_point(aes(x = 0, y = tau, shape = sig), colour = gg_color_hue(2)[1], 
+                vplot <- vplot  + 
+                    geom_point(aes(x = 0, y = tau, shape = sig, colour = stat), 
                                       position = position_jitter(w = 0.1, h = 0), size = 3, alpha = .7) +
                     scale_shape_manual(values = c(21, 16)) 
-                
-                v2 <- v2 + 
-                    geom_point(aes(x = 0, y = tau, shape = sig), colour = gg_color_hue(2)[2], 
-                               position = position_jitter(w = 0.1, h = 0), size = 3, alpha = .7) +
-                    scale_shape_manual(values = c(21, 16))
             }else{
-                v1 <- v1 + 
-                    geom_point(aes(x = 0, y = tau), colour = gg_color_hue(2)[1], 
+                vplot <- vplot + 
+                    geom_point(aes(x = 0, y = tau, colour = stat), 
                                position = position_jitter(w = 0.1, h = 0), size = 3, alpha = .7) 
-                v2 <- v2 + 
-                    geom_point(aes(x = 0, y = tau), colour = gg_color_hue(2)[2], 
-                                      position = position_jitter(w = 0.1, h = 0), size = 3, alpha = .7)
             }
             
-            
-            
-                
-            
-            vplot <- gridExtra::grid.arrange(v1, v2, ncol = 2, top = "Violin Plot - tau estimates for Cronbach's Alpha")
             
         }
         
